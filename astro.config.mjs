d0237ac5bd7b65790d705370fa7844e36e9799a7
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-23 23:10:28 +0700

    feat: tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })],
   site: 'https://katakatari.github.io',
   base: '/gh-astro-01',
 });

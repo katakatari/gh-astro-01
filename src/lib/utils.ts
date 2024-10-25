061223b591ae272f104aaccad5bec55761bef1aa
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-26 00:31:34 +0700

    setup: helper functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 }
+
+const getBaseUrl = () => "/gh-astro-01"
+const getBaseDomain = () => "https://katakatari.github.io"
+const getFullBaseUrl = () => getBaseDomain() + getBaseUrl()
+
+export { cn, getBaseUrl, getBaseDomain, getFullBaseUrl }

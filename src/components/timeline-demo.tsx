24f6cdafd178e3551003e83bd431baf1e16d7a22
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-26 16:07:16 +0700

    refactor: make timeline component more reusable and flexible

--- src/components/timeline-demo.tsx
+++ src/components/timeline-demo.tsx
@@ -1,4 +1,4 @@
-import { Timeline } from "@components/with-motion/timeline";
+import Timeline from "@components/with-motion/timeline";
 import FocusCard from "@components/with-motion/focus-card";
 
 const data = [
@@ -13,40 +13,32 @@ const data = [
           vitae eos?
         </p>
         <div className="grid grid-cols-2 gap-4">
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of mountains on gloomy day",
-            }}
-          />
+          <FocusCard title="Pictures by Pexels\nfree photo of mountains on gloomy day">
+            <img
+              src="https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of mountains on gloomy day"
+            />
+          </FocusCard>
 
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of weeds on beach on shore",
-            }}
-          />
+          <FocusCard title="Pictures by Pexels\nfree photo of weeds on beach on shore">
+            <img
+              src="https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of weeds on beach on shore"
+            />
+          </FocusCard>
 
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of mountains on gloomy day",
-            }}
-          />
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of mountains on gloomy day",
-            }}
-          />
+          <FocusCard title="Pictures by Pexels\nfree photo of weeds on beach on shore">
+            <img
+              src="https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of weeds on beach on shore"
+            />
+          </FocusCard>
+          <FocusCard title="Pictures by Pexels\nfree photo of mountains on gloomy day">
+            <img
+              src="https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of mountains on gloomy day"
+            />
+          </FocusCard>
         </div>
       </div>
     ),
@@ -62,40 +54,32 @@ const data = [
           nemo corrupti quo esse minus!
         </p>
         <div className="grid grid-cols-2 gap-4">
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of mountains on gloomy day",
-            }}
-          />
+          <FocusCard title="Pictures by Pexels\nfree photo of mountains on gloomy day">
+            <img
+              src="https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of mountains on gloomy day"
+            />
+          </FocusCard>
 
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of weeds on beach on shore",
-            }}
-          />
+          <FocusCard title="Pictures by Pexels\nfree photo of weeds on beach on shore">
+            <img
+              src="https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of weeds on beach on shore"
+            />
+          </FocusCard>
 
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of mountains on gloomy day",
-            }}
-          />
-          <FocusCard
-            data={{
-              src:
-                "https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-              title:
-                "Pictures by Pexels\nfree photo of mountains on gloomy day",
-            }}
-          />
+          <FocusCard title="Pictures by Pexels\nfree photo of weeds on beach on shore">
+            <img
+              src="https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of weeds on beach on shore"
+            />
+          </FocusCard>
+          <FocusCard title="Pictures by Pexels\nfree photo of mountains on gloomy day">
+            <img
+              src="https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+              alt="Pictures by Pexels\nfree photo of mountains on gloomy day"
+            />
+          </FocusCard>
         </div>
       </div>
     ),
@@ -104,43 +88,65 @@ const data = [
     title: "Gallery",
     content: (
       <div className="grid grid-cols-2 gap-4">
-        <FocusCard
-          data={{
-            src:
-              "https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Pictures by Pexels\nfree photo of mountains on gloomy day",
-          }}
-        />
+        <FocusCard title="Pictures by Pexels\nfree photo of mountains on gloomy day">
+          <img
+            src="https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="Pictures by Pexels\nfree photo of mountains on gloomy day"
+          />
+        </FocusCard>
 
-        <FocusCard
-          data={{
-            src:
-              "https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Pictures by Pexels\nfree photo of weeds on beach on shore",
-          }}
-        />
+        <FocusCard title="Pictures by Pexels\nfree photo of weeds on beach on shore">
+          <img
+            src="https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="Pictures by Pexels\nfree photo of weeds on beach on shore"
+          />
+        </FocusCard>
 
-        <FocusCard
-          data={{
-            src:
-              "https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Pictures by Pexels\nfree photo of mountains on gloomy day",
-          }}
-        />
-        <FocusCard
-          data={{
-            src:
-              "https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-            title: "Pictures by Pexels\nfree photo of mountains on gloomy day",
-          }}
-        />
+        <FocusCard title="Pictures by Pexels\nfree photo of weeds on beach on shore">
+          <img
+            src="https://images.pexels.com/photos/28075061/pexels-photo-28075061/free-photo-of-weeds-on-beach-on-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="Pictures by Pexels\nfree photo of weeds on beach on shore"
+          />
+        </FocusCard>
+        <FocusCard title="Pictures by Pexels\nfree photo of mountains on gloomy day">
+          <img
+            src="https://images.pexels.com/photos/18795422/pexels-photo-18795422/free-photo-of-mountains-on-gloomy-day.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt="Pictures by Pexels\nfree photo of mountains on gloomy day"
+          />
+        </FocusCard>
       </div>
     ),
   },
 ];
 
 const TimelineDemo = () => {
-  return <Timeline data={data} />;
+  return (
+    <Timeline>
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className="flex justify-start pt-10 md:pt-40 md:gap-10"
+        >
+          <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+            <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full flex items-center justify-center">
+              <div className="h-4 w-4 rounded-full bg-foreground border border-background p-2" />
+            </div>
+            <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500">
+              {item.title}
+            </h3>
+          </div>
+
+          <div className="relative pl-20 pr-4 md:pl-4 w-full">
+            <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500">
+              {item.title}
+            </h3>
+            {item.content}
+            {" "}
+          </div>
+        </div>
+      ))}
+    </Timeline>
+  );
 };
 
 export default TimelineDemo;
--- src/components/with-motion/timeline.tsx
+++ src/components/with-motion/timeline.tsx
@@ -3,13 +3,17 @@ import { cn } from "@lib/utils";
 import { motion, useScroll, useTransform } from "framer-motion";
 import React from "react";
 
-interface TimelineEntry {
-  title: string;
-  content: React.ReactNode;
+interface TimelineProps {
+  children: React.ReactNode;
+  header?: {
+    title: string;
+    description?: string;
+  };
+  className?: string;
 }
 
-export const Timeline = (
-  { data, className }: { data: TimelineEntry[]; className?: string },
+const Timeline = (
+  { children, className, header }: TimelineProps,
 ) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const containerRef = React.useRef<HTMLDivElement>(null);
@@ -38,42 +42,23 @@ export const Timeline = (
       )}
       ref={containerRef}
     >
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 max-w-4xl">
-          Changelog from my journey
-        </h2>
-        <p className="text-sm md:text-base max-w-sm">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia error
-          iure non dolores consectetur ea, dolorum ipsum nihil maxime cupiditate
-          officiis vitae rem, aliquam aperiam natus quas suscipit! Nemo,
-          veritatis!
-        </p>
-      </div>
+      {/* Can be reusable in the future when needs arrives */}
+      {header && (
+        <header className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+          <h2 className="text-lg md:text-4xl m{b-4 max-w-4xl">
+            {header?.title}
+          </h2>
+
+          {header?.description && (
+            <p className="text-sm md:text-base max-w-sm">
+              {header?.description}
+            </p>
+          )}
+        </header>
+      )}
 
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
-        {data.map((item, index) => (
-          <div
-            key={index}
-            className="flex justify-start pt-10 md:pt-40 md:gap-10"
-          >
-            <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-foreground border border-background p-2" />
-              </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500">
-                {item.title}
-              </h3>
-            </div>
-
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500">
-                {item.title}
-              </h3>
-              {item.content}
-              {" "}
-            </div>
-          </div>
-        ))}
+        {children}
         <div
           style={{
             height: height + "px",
@@ -92,3 +77,5 @@ export const Timeline = (
     </div>
   );
 };
+
+export default Timeline

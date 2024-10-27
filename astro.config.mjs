255757982fcb1e386af372bd79dd0fb3761948c5
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-28 00:53:26 +0700

    setup: mdx;feat: simple layout for initial blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react()],
+  }), mdx(), react()],
   site: 'https://katakatari.github.io',
   base: '/gh-astro-01',
 });

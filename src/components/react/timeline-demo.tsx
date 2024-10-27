cce83adbe5fad44f76131ff0e153473eeffdd54c
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-27 13:02:16 +0700

    refactor: move big not reusable components to the specific directory based on its extension

--- src/components/react/timeline-demo.tsx
+++ src/components/react/timeline-demo.tsx
@@ -1,29 +1,6 @@
 import Timeline from "@components/with-motion/timeline";
 import FocusCard from "@components/with-motion/focus-card";
-import { SiAstro, SiGithub, SiReact, SiTailwindcss } from "react-icons/si";
-import type { IconBaseProps } from "react-icons/lib";
-import { LuCode2 } from "react-icons/lu";
-import { getBaseUrl } from "@lib/utils";
-
-const url = getBaseUrl();
-const Icons = {
-  github: SiGithub,
-  astro: SiAstro,
-  react: SiReact,
-  tailwind: SiTailwindcss,
-  code: LuCode2,
-} as const;
-
-type IconName = keyof typeof Icons;
-interface IconProps extends IconBaseProps {
-  name: IconName;
-}
-
-const Icon = ({ name, ...props }: IconProps) => {
-  const Icon = Icons[name];
-
-  return <Icon className="size-6" {...props} />;
-};
+import Icon from "../ui/icon";
 
 const techStacks = ["github", "astro", "react", "tailwind"] as const;
 

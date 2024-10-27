4c594ff50c9956d5312eb6ee86666830c11d29c6
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-27 15:00:23 +0700

    refactor: remove primitive components in favor of using css reset

--- src/components/primitives/header.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Header = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ ...props }, ref) => <header ref={ref} {...props} />);
-
-export default Header;
--- src/components/primitives/nav.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Nav = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ ...props }, ref) => <nav ref={ref} {...props} />);
-
-export default Nav;

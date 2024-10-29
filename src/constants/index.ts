fad96ffe49844f687f0a04a206f59d8bd3ccc1cb
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-10-29 22:29:07 +0700

    feat: simple contact form and toast after submitting the form

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ const NAVIGATIONS = [
   { href: "gallery", label: "Gallery" },
   { href: "blog", label: "Blog" },
   { href: "code-journey", label: "Code" },
-  // { href: "contact", label: "Contact" },
+  { href: "contact", label: "Contact" },
 ];
 
 export { NAVIGATIONS };

28bf01e01826c7f4a5c41815a5d4cdf9e6e935c1
Bayu Prasetya Utomo <108232231+BayuPrasetyaUtomo@users.noreply.github.com>
2024-11-01 20:38:54 +0700

    feat: icodethis users card challenge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,8 +5,14 @@ const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 }
 
-const getBaseUrl = () => "/gh-astro-01"
-const getBaseDomain = () => "https://katakatari.github.io"
-const getFullBaseUrl = () => getBaseDomain() + getBaseUrl()
+const getBaseUrl = () => "/gh-astro-01";
+const getBaseDomain = () => "https://katakatari.github.io";
+const getFullBaseUrl = () => getBaseDomain() + getBaseUrl();
 
-export { cn, getBaseUrl, getBaseDomain, getFullBaseUrl }
+const getRandomIntInclusive = (min: number, max: number) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+export { cn, getBaseUrl, getBaseDomain, getFullBaseUrl, getRandomIntInclusive }
